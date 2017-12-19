0ecb678d2e2ac2122e1452a49f7ba15e23aaa475
Keith MacKay <keith.mackay@q-free.com>
2017-12-19 12:35:50 -0500

    formatting

--- files.py
+++ files.py
@@ -2,6 +2,22 @@ import os
 import sys
 import re
 
+
+def bytes_to_readable(byte_count: int) -> str:
+    unit = {
+        0: "B ",
+        1: "KB",
+        2: "MB",
+        3: "GB",
+        4: "TB",
+        5: "PB"
+    }
+    x = 0
+    while byte_count > 1024:
+        x+=1
+        b /= 1024
+
+
 if __name__ == "__main__":
 
     biggest = ("", -1)
@@ -9,16 +25,16 @@ if __name__ == "__main__":
     def search(dir, match):
         global biggest
         for item in os.listdir(dir):
-                item = os.path.join(dir, item)
-                if os.path.isdir(item):
-                    search(item, match)
-                elif match.match(item):
-                    itemsize = os.path.getsize(item)
-                    if itemsize > biggest[1]:
-                        biggest = (item, itemsize)
-                        # print(f"New Largest file: {itemsize} bytes - \"{item}\"")
+            item = os.path.join(dir, item)
+            if os.path.isdir(item):
+                search(item, match)
+            elif match.match(item):
+                itemsize = os.path.getsize(item)
+                if itemsize > biggest[1]:
+                    biggest = (item, itemsize)
+                    # print(f"New Largest file: {itemsize} bytes - \"{item}\"")
 
     search(sys.argv[1] if len(sys.argv) > 1 else "C:\\projects\\opentms",
            sys.argv[2] if len(sys.argv) > 2 else re.compile(".*"))
 
-    print(f"\n\nLargest File: {biggest[0]} - {biggest[1]} bytes")
+    print(f"\n\nLargest File: {biggest[0]} - {bytes_to_readable(biggest[1]})")
