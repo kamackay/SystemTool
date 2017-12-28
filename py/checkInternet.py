e15e2d5bbc6177c7af89ec41578cf2b9f4efcddc
Keith MacKay <keith.mackay@q-free.com>
2017-12-28 14:57:33 -0500

    now logs

--- py/checkInternet.py
+++ py/checkInternet.py
@@ -1,11 +1,14 @@
 from urllib import request
 import time
 import datetime
+import sys
 
 
-def log(*args):
-    args = [datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S -')] + list(args)
-    print(*tuple(args))
+def log(s: str = ""):
+    line = f"{datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S - ')} {s}"
+    print(line)
+    with open("c:\\internet.log", "a") as f:
+        f.write(line + "\n")
 
 
 def internet_on(timeout: int = 1):
@@ -17,13 +20,15 @@ def internet_on(timeout: int = 1):
 
 
 if __name__ == "__main__":
+    verbose = "--v" in sys.argv
+    log("Now checking internet status")
     try:
         while True:
             if not internet_on():
                 log("No Internet!")
                 while not internet_on(timeout=10):
                     time.sleep(1)  # wait for the Internet to come back
-            else:
+            elif verbose:
                 log("Internet is fine")
             time.sleep(10)
     except:
